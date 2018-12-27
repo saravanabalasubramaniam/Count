e89406eb85706591a0acde0630d40dce747d4fd9
saravana <saravanasaru14313@gmail.com>
2018-12-27 07:23:11 -0800

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Count
+{
+public static void main(String args[])
+    {
+     String name="hello";
+     String name1="world";
+     System.out.println("the string is  "  +name);
+     System.out.println("the string is  "  +name1);
+     String name2=name+name1;
+     System.out.println("the string is  "  +name2);
+     System.out.println("number of charaters are  "  +name2.length());
+     }
+}
+ 
